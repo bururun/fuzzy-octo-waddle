fa90cd29d970613956ed2f6d275ed1e84603c5bc
bururun <bururun@users.noreply.github.com>
2025-09-09 07:40:30 +0300

    chore: fix broken tests

--- lib/helpers.py
+++ lib/helpers.py
@@ -29,3 +29,14 @@ def helper_function_11(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_32(x):
+    """Helper function for iteration 32."""
+    return x * 32
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
